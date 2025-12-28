edd81710cbd1cde728a24be18aa246f63b839abb
Madureira <rafael.madureira.santos@gmail.com>
2025-12-27 23:49:12 -0300

    Fix c and cpp syntax highlight

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,7 +4,21 @@ return {
     picker = {
       sources = {
         explorer = {
-          explorer = { enabled = true },
+          explorer = {
+            enabled = true,
+            hidden = true,
+            auto_close = false,
+            win = {
+              list = {
+                keys = {
+                  ["-"] = "edit_split",
+                  ["|"] = "edit_vsplit",
+                  ["."] = "explorer_focus",
+                  ["O"] = { { "pick_win", "jump" }, mode = { "n", "i" } },
+                },
+              },
+            },
+          },
           input = { enabled = false },
           auto_close = false,
           jump = { close = false },
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      require("nvim-treesitter.install").compilers = { "clang", "cc", "gcc" }
+    end,
+    config = function()
+      require("nvim-treesitter").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = {
+          "c",
+          "cpp",
+          "lua",
+          "javascript",
+          "rust",
+        },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+        auto_install = true,
+
+        folds = { enable = true },
+
+        indent = {
+          enable = true,
+        },
+
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
+          additional_languages = {},
+        },
+
+        textobjects = { enable = true },
+
+        additional_vim_regex_highlighting = { "markdown" },
+      })
+
+      require("nvim-treesitter").install({
+        "c",
+        "cpp",
+        "lua",
+        "javascript",
+        "rust",
+      })
+    end,
+  },
+}
